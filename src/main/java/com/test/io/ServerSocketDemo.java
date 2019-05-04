f536579e46203a88e3f4003dbd0c9a435521f87a
zhiweiluo6 <1741520510@qq.com>
2019-05-04 13:40:23 +0800

    已经去掉发送文件的功能--仅加注释--测试成功

--- src/main/java/com/test/io/ServerSocketDemo.java
+++ src/main/java/com/test/io/ServerSocketDemo.java
@@ -13,45 +13,45 @@ public class ServerSocketDemo {
 
         //  2、使用while循环不停的接收客户端发送的请求
         while(true){
-            //  3、调用ServerSocket对象accept（）方法接收客户端的请求，得到Socket对象
+            //  3、调用ServerSocket对象accept（）方法接收客户端发送的请求，得到Socket对象
             Socket socket =  serverSocket.accept();
             System.out.println("有client连接进入...");
-            // 4. 分配线程处理会话
-            new Thread(new ServerChatHandler(socket)).start();
+            // 4. 分配线程处理会话   服务器端为每个来访问的客户端创建一个对应的Socket，并且开启一个新的线程使两个Socket建立专线进行通信。
+            new Thread(new ServerChatHandler(socket)).start();  // ServerChatHandler 服务器聊天处理器
         }
     }
 }
+//开启的新线程是通过实现Runnable接口创建的，重写的run()方法中实现了服务器端接收、发送数据的功能。
+class ServerChatHandler implements Runnable {    //聊天处理线程
 
-class ServerChatHandler implements Runnable {
+    private Socket socket;          //持有一个Socket类型的属性
 
-    private Socket socket;
-
-    public ServerChatHandler(Socket socket){
+    public ServerChatHandler(Socket socket){    //构造方法中把Socket对象作为实参传入
         this.socket = socket;
     }
 
     public void run() {
         OutputStream outputStream  = null;
         InputStream inputStream = null;
-        //创建Scanner对象
-        Scanner sc = new Scanner(System.in);//System.in表示标准化输出，也就是键盘输出
+        //创建Scanner类的实例化对象 （使用Scanner类实现数据输入）
+        Scanner sc = new Scanner(System.in);//用于读取键盘输入的数据   //System.in表示标准化输入
         try {
-            outputStream = socket.getOutputStream();
-            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();   //服务端的Socket对象上的getOutputStream方法得到的输出流其实就是发送给客户端的数据。
+            inputStream = socket.getInputStream();   // 服务端的Socket对象上的getInputStream方法得到的输入流其实就是从客户端发送给服务器端的数据流。
 
-            byte[] content = new byte[1024];
-            int len = 0;
-            while ((len = inputStream.read(content)) != -1) {
-                byte[] tempBytes = new byte[len];
+            byte[] content = new byte[1024];   //定义一个字节数组  接收客户端发来的数据  容量大小可调节
+            int len = 0;        //定义一个int类型的变量len ,初始值为0
+            while ((len = inputStream.read(content)) != -1) {  //读取已装进content字节数组中的数据，并赋值给len
+                byte[] tempBytes = new byte[len];     // 把实际接收到的数据"长度 "再读进容器定义，只作为长度，不包括内容
                 for (int i = 0; i < len; i++) {
-                    tempBytes[i] = content[i];
+                    tempBytes[i] = content[i];   //将实际接收到的数据内容 平移 装进容器   字节数组
                 }
 
-                String str = new String(tempBytes);
-                System.out.println("client：" + str);
+                String str = new String(tempBytes);  //将字节数组转换为字符串  解码
+                System.out.println("client：" + str);   //将实际接收到的数据（客户端发来的数据） 以字符串形式输出
 
                 // 如果"#end#"， 意味着client想终止会话。
-                if ("#END#".equals(str)) {
+                if ("#END#".equals(str)) {    //比较字符串与“”的值是否相等
                     socket.close();//关闭连接
                     System.out.println("结束与客户端交互数据");
                     return;//退出函数, 则线程结束
@@ -59,16 +59,17 @@ class ServerChatHandler implements Runnable {
 
                 System.out.print("我：");
                 //利用hasNextXXX()判断是否还有下一输入项
-                String answerStr = sc.nextLine();
-                outputStream.write(answerStr.getBytes());
-                outputStream.flush();
+                String answerStr = sc.nextLine();  //使用Scanner类中的nextLine()可以扫描到一行内容并实现字符串String的获取。
+                outputStream.write(answerStr.getBytes());  //String的getBytes()方法是得到一个操作系统默认的编码格式的字节数组。
+                outputStream.flush();       //flush() 方法刷新此输出流并强制将所有缓冲的输出字节被写出
             }
-        } catch (IOException e) {
+        } catch (IOException e) {    //输入或输出异常
             e.printStackTrace();
+
         } finally {
             try {
                 socket.close();
-            } catch (IOException e) {
+            } catch (IOException e) {    //输入或输出异常
                 e.printStackTrace();
             }
         }
--- src/main/java/com/test/io/SocketDemo.java
+++ src/main/java/com/test/io/SocketDemo.java
@@ -12,15 +12,17 @@ public class SocketDemo {
             client = new Socket("localhost", 1025);
             System.out.println("成功连接server...");
 
-            InputStream socketIns = client.getInputStream();  //得到接收数据的流
-            OutputStream socketOut = client.getOutputStream();//获取发送数据的流
+            InputStream socketIns = client.getInputStream();  //用户先输入，让client读取/识别  客户端的Socket对象上的getInputStream方法得到输入流其实就是从服务器端发回的数据。
+            OutputStream socketOut = client.getOutputStream();//获取发送数据的流   客户端的Socket对象上的getOutputStream方法得到的输出流其实就是发送给服务器端的数据。
 
             // 开启新的一条线程接收server发送过来的数据
             ClientChatHandler clientChatHandler = new ClientChatHandler(socketIns);
             new Thread(clientChatHandler).start();
 
-            Scanner scanner = new Scanner(System.in);//读取键盘输入
-            boolean chatIsNotOver = true;
+            //创建Scanner类的实例化对象  用于读取客户端用户键盘输入的数据 （使用Scanner类实现数据输入）
+            Scanner scanner = new Scanner(System.in);
+
+            boolean chatIsNotOver = true;       //判断聊天没结束的值是true
             while (chatIsNotOver) {
                 while (!clientChatHandler.getHasAnswer()) {
                     synchronized (clientChatHandler) {
@@ -30,11 +32,11 @@ public class SocketDemo {
 
                 System.out.print("我：");
                 // client发送第一个问话
-                String askStr = scanner.nextLine();
+                String askStr = scanner.nextLine();  //使用Scanner类中的nextLine()可以扫描到一行内容并实现字符串String的获取。
 
                 if (askStr.equals("#endChat#")) {//用户想结束会话
-                    askStr = "#END#";   //发给服务器
-                    socketOut.write(askStr.getBytes());
+                    askStr = "#END#";   //发给服务器     重新再赋值
+                    socketOut.write(askStr.getBytes());   //String的getBytes()方法是得到一个操作系统默认的编码格式的字节数组。
                     // 提示子线程：聊天已经结束了
                     clientChatHandler.setOver(true);
 
@@ -42,70 +44,68 @@ public class SocketDemo {
                 }
 
                 // 其余的聊天和请求文件 都是直接发送过去就行了！
-                socketOut.write(askStr.getBytes());
+                socketOut.write(askStr.getBytes());  //String的getBytes()方法是得到一个操作系统默认的编码格式的字节数组。
 
-                // 设置当前聊天内容尚未回复
+                // 设置当前聊天内容尚未 回复
                 synchronized (clientChatHandler) {
-                    clientChatHandler.setHasAnswer(false);
+                    clientChatHandler.setHasAnswer(false);  //false 未 回复
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException e) {     //输入或输出异常
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (InterruptedException e) {    //中断异常
             e.printStackTrace();
-        } catch (Exception e) {
+        } catch (Exception e) {    //wait()
             e.printStackTrace();
         } finally {
-            if (client != null) {
-                try {
-                    client.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            if (client != null) {  //null表示没有指向任何字符
+                    try {
+                        client.close();   //释放资源
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
             }
         }
     }
 }
-
+//开启的新线程是通过实现Runnable接口创建的，重写的run()方法中实现了客户端  只做接收数据 ，并将接收的数据打印出来。
 class ClientChatHandler implements Runnable {
     private InputStream socketIns;
-    private boolean isOver = false;//初始化 标识socket是否已经结束
-    private volatile boolean hasAnswer = true;// 标识当前ask是否已经有回复
+    private boolean isOver = false;//初始化 标识socket是否已经结束  已结束
+    private volatile boolean hasAnswer = true;// 标识当前ask是否已经 有回复
 
     public ClientChatHandler(InputStream inputStream) {
         this.socketIns = inputStream;
     }
 
-    public void setOver(boolean over) {   //
+    public void setOver(boolean over) {
         isOver = over;
     }
 
-    public void setHasAnswer(boolean hasAnswer) {
+    public void setHasAnswer(boolean hasAnswer) {   //初始化 设置是否有回复
         this.hasAnswer = hasAnswer;
     }
-
-    public boolean getHasAnswer() {
+    public boolean getHasAnswer() {   //获取回复
         return hasAnswer;
     }
 
-    @Override
     public void run() {
-        while (!isOver) {// 一直循环，直到收到通知
+        while (!isOver) {// 一直循环，直到收到服务器端的通知
             try {
-                // 一次读入当前接收到的所有数据
-                byte[] bytes = new byte[1024 * 1024 * 10];
-                int getDataLength = socketIns.read(bytes);
+                // 一次读入当前接收到的所有数据       （来自服务器的数据）
+                byte[] bytes = new byte[1024 * 1024 * 10];   //接收数据的最大限度
+                int getDataLength = socketIns.read(bytes);      //实际接收到的数据长度
 
-                byte[] chatContent = new byte[getDataLength];
+                byte[] chatContent = new byte[getDataLength];   // 把实际接收到的数据 长度 再读进容器定义，只作为长度，不包括内容
                 for (int i = 0; i < getDataLength; i++) {
-                    chatContent[i] = bytes[i];
+                    chatContent[i] = bytes[i];          //将实际接收到的数据内容平移装进容器   字节数组
                 }
-                System.out.println("server：" + new String(chatContent));
+                System.out.println("server：" + new String(chatContent));  //将字节数组转换为字符串
 
                 // 聊天已收到回复
-                synchronized (this) {
+                synchronized (this) {   //this指向当前调用它的对象
                     this.setHasAnswer(true);
-                    this.notify();
+                    this.notify();   //notify()的调用者是同步锁对象
                 }
             } catch (Exception e) {
                 System.out.println("连接已经断开...");
